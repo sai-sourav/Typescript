3dca4a63d33b32161057e3b71b7f638aaeb6383c
sai-sourav <saisourav123@gmail.com>
2022-12-17 19:47:41 +0530

    type casting of requestbody

--- routes/todo.ts
+++ routes/todo.ts
@@ -6,6 +6,9 @@ const router = Router();
 
 const Todos: Todo[] = [];
 
+type Requestbody = {  id : string,
+                      text: string };
+
 router.post('/todo', (req,res,next) => {
     const newTodo: Todo = {
 		id: new Date().toISOString(),
@@ -16,13 +19,8 @@ router.post('/todo', (req,res,next) => {
 })
 
 router.post('/delete', (req, res, next) => {
-    const elementid = req.body.id;
-    // let index;
-    // for(let i= 0; i< Todos.length; i++){
-    //     if(Todos[i].id === elementid){
-    //         index = i;
-    //     }
-    // }
+    const body = req.body as Requestbody;
+    const elementid = body.id;
     const index = Todos.findIndex(todoitem => todoitem.id === elementid);
     if(index !== -1){
         Todos.splice(index, 1);
@@ -33,17 +31,11 @@ router.post('/delete', (req, res, next) => {
 });
 
 router.post('/update', (req, res, next) => {
-    const elementid = req.body.id;
+    const body = req.body as Requestbody;
+    const elementid = body.id;
     const todoindex = Todos.findIndex(todoitem => todoitem.id === elementid);
-    // let index;
-    // for(let i= 0; i< Todos.length; i++){
-    //     if(Todos[i].id === elementid){
-    //         Todos[i].text = req.body.text;
-    //         return res.status(200).json({ todos: Todos });
-    //     }
-    // }
     if(todoindex !== -1){
-        Todos[todoindex].text = req.body.text;
+        Todos[todoindex].text = body.text;
         res.status(200).json({ todos: Todos });
     } else {
         res.status(404).json({ todos: Todos });
