246513e3dbdba1cf2ed20b4f33751959fd7c4afe
sai-sourav <saisourav123@gmail.com>
2022-12-17 18:39:14 +0530

    bug fix

--- routes/todo.js
+++ routes/todo.js
@@ -13,13 +13,14 @@ router.post('/todo', (req, res, next) => {
 });
 router.post('/delete', (req, res, next) => {
     const elementid = req.body.id;
-    let index;
-    for (let i = 0; i < Todos.length; i++) {
-        if (Todos[i].id === elementid) {
-            index = i;
-        }
-    }
-    if (index !== undefined) {
+    // let index;
+    // for(let i= 0; i< Todos.length; i++){
+    //     if(Todos[i].id === elementid){
+    //         index = i;
+    //     }
+    // }
+    const index = Todos.findIndex(todoitem => todoitem.id === elementid);
+    if (index !== -1) {
         Todos.splice(index, 1);
         res.status(200).json({ todos: Todos });
     }
@@ -29,14 +30,21 @@ router.post('/delete', (req, res, next) => {
 });
 router.post('/update', (req, res, next) => {
     const elementid = req.body.id;
-    let index;
-    for (let i = 0; i < Todos.length; i++) {
-        if (Todos[i].id === elementid) {
-            Todos[i].text = req.body.text;
-            return res.status(200).json({ todos: Todos });
-        }
+    const todoindex = Todos.findIndex(todoitem => todoitem.id === elementid);
+    // let index;
+    // for(let i= 0; i< Todos.length; i++){
+    //     if(Todos[i].id === elementid){
+    //         Todos[i].text = req.body.text;
+    //         return res.status(200).json({ todos: Todos });
+    //     }
+    // }
+    if (todoindex !== -1) {
+        Todos[todoindex].text = req.body.text;
+        res.status(200).json({ todos: Todos });
     }
-    res.status(404).json({ todos: Todos });
+    else {
+        res.status(404).json({ todos: Todos });
+    }
 });
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: Todos });
